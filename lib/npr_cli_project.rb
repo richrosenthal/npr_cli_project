c6a8da4c68d65146158922c2e2d6ab59ef088933
Richard Rosenthal <richard.rosenthal@acd.ccac.edu>
2020-01-20 02:20:26 +0000

    corrected require_relative requirements

--- lib/npr_cli_project.rb
+++ lib/npr_cli_project.rb
@@ -1,6 +1,8 @@
-require "npr_cli_project/version"
+require_relative "npr_cli_project/version"
 
 module NprCliProject
   class Error < StandardError; end
   # Your code goes here...
 end
+
+require_relative './npr_cli_project/CLI'
--- lib/npr_cli_project/CLI.rb
+++ lib/npr_cli_project/CLI.rb
@@ -1,4 +1,8 @@
-class NPR_CLI::CLI 
+class NprCliProject::CLI 
+  
   def call 
-    puts "Hello World"
+    puts "Welcome to All Gems Considered"
+    puts "Your number one place for all NPR daily news"
+  end 
+  
 end 
