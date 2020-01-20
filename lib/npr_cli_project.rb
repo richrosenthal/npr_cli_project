195f5dc915cd3dd66eff2f4cf744d10e00a77d7f
Richard Rosenthal <richard.rosenthal@acd.ccac.edu>
2020-01-20 02:26:11 +0000

    set ./lib/npr_cli_project.rb as environment

--- lib/npr_cli_project.rb
+++ lib/npr_cli_project.rb
@@ -1,8 +1,9 @@
-require_relative "npr_cli_project/version"
-
-module NprCliProject
-  class Error < StandardError; end
-  # Your code goes here...
-end
-
+require_relative "./npr_cli_project/version"
 require_relative './npr_cli_project/CLI'
+
+
+# module NprCliProject
+#   # class Error < StandardError; end
+#   # Your code goes here...
+# end
+
