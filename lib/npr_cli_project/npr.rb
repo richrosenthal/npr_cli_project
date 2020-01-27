55075c1f9b78f603bc9c45c6bbddc657aae588eb
Richard Rosenthal <richard.rosenthal@acd.ccac.edu>
2020-01-27 19:43:05 +0000

    fixed infinite loop in selection method

--- lib/npr_cli_project/npr.rb
+++ lib/npr_cli_project/npr.rb
@@ -1,50 +1,87 @@
 require 'pry'
+#Author Richard Rosenthal Janaury 27 2020
+# “They blew out a breath and did the thing all heroes must do—they took that terrifying first step.”
+# ― Chloe Neill, Midnight Marked
 
-#Author Richard Rosenthal 2020
-
-class NprCliProject::NPR
+class NprCliProject::CLI 
   
-  attr_accessor :headline, :summary 
-  @@all = [] #class variable
- 
- 
-  
-  # def save 
-  #   @@all << self 
-  # end 
-  
-  
-  #self .all method will return the class variable @@all
-  
-  def self.all 
-    @@all 
+  #call method starts the CLI class 
+  def call 
+    
+    # puts <<-'EOF'
+    # _        _______  _______    _______  _______  _______ 
+    # ( (    /|(  ____ )(  ____ )  (  ____ \(  ____ \(       )
+    # |  \  ( || (    )|| (    )|  | (    \/| (    \/| () () |
+    # |   \ | || (____)|| (____)|  | |      | (__    | || || |
+    # | (\ \) ||  _____)|     __)  | | ____ |  __)   | |(_)| |
+    # | | \   || (      | (\ (     | | \_  )| (      | |   | |
+    # | )  \  || )      | ) \ \__  | (___) || (____/\| )   ( |
+    # |/    )_)|/       |/   \__/  (_______)(_______/|/     \|
+    
+    
+    
+    
+    # EOF
+    
+    
+    puts "Welcome to All Gems Considered"
+    puts "Your number one place for all NPR daily news"
+    puts " "
+    
+    list_headlines
+    selection 
+    goodbye
   end 
   
-
-  #.self.stories method calls the .make_stories method and returns class variable array.
+  #list_headlines method will take the scraper object created from the NPR class and iterate over all of it's headline elements.
   
-  # def self.stories
-  #   self.make_stories
-  # @@all
-  # end 
-  
-  
-  #.make_stories is scraping NPR website and creating a story object that will be shoveled into the class variable.
-  
-  def self.make_stories
-   doc = Nokogiri::HTML(open("https://www.npr.org/sections/news/"))
-   stories = doc.css("div.item-info")
-    stories.each do |item|
-      story = self.new
-      story.headline = item.search("h2.title").text.strip
-      story.summary = item.search("p.teaser").text.strip
-      @@all << story 
-    end
-    @@all
-   end 
-   
-  
-
+  def list_headlines
+    
+    
+   @news = NprCliProject::NPR.make_stories
+   #binding.pry  
  
+  count = 1 
+  @news.each do  |news_stories|
+      puts "#{count}. " + news_stories.headline  
+      count +=1 
+    end 
+    puts ""
+  end 
+  
+  
+  #selection method will print out the headlines 
+  
+  def selection 
+    puts "Enter the number of the headline to read its summary"
+    input = nil 
+    while input != "exit"
+    input = gets.strip.downcase
+    new_stories = nil
+     if input.to_i > 0 && input.to_i <= @news.length 
+        news_stories = @news[input.to_i-1]
+         puts "#{news_stories.summary}"
+         puts ""
+         puts "Type list to go back or exit to quit program"
+         input2 = gets.strip.downcase
+         if input2 == "list"
+           selection
+         elsif input2 == "exit"
+            input = "exit"
+        end 
+      else puts "I do not understand, please try again."
+      end 
+    end 
+  end 
+  
+  # goodbye method will exit the program after confirming with the user if they would really want to exit
+  
+  def goodbye 
+    puts "Wait wait are you sure? (Yes/No)"
+    input = gets.strip.downcase
+      if input == "no"
+        selection 
+      end 
+  end 
   
 end 
